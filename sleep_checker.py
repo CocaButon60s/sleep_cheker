b34858bf568589f477b8713e3cadfb86c86ba0c5
Hiroyuki Ikuno <sam2kaikaramegusuri@gmail.com>
2020-12-12 01:15:50 +0900

    ファイル名変更 処理修正

---
+++ sleep_checker.py
@@ -0,0 +1,105 @@
+import cv2
+import winsound
+import subprocess
+from datetime import datetime, timedelta
+from time import sleep
+
+# チャタ用
+class Chattering():
+    def __init__(self):
+        self.__buf = [False, False, False]
+        self.__p = 0
+    
+    def add(self, val):
+        self.__buf[self.__p] = val
+        self.__p = self.__p + 1 if self.__p + 1 < len(self.__buf) else 0
+
+    def is_allTrue(self):
+        return False not in self.__buf
+
+# 経過時間管理
+class TimeMng():
+    def __init__(self):
+        self.__start = None
+    
+    def update(self):
+        self.__start = datetime.now()
+
+    def get_timedelta(self, now: datetime) -> timedelta:
+        return now - self.__start
+
+    def is_over(self, now: datetime) -> bool:
+        # 5分以上動きがなかった
+        if (now - self.__start >= timedelta(minutes=5)): return True
+        return False
+
+def alerm(f):
+    print("alerm")
+    # 音量調整
+    subprocess.call("tools\SetVol\SetVol.exe 18", shell=True)
+    while True:
+        winsound.Beep(f, 1000)
+        k = cv2.waitKey(1000)
+        # Esc押されたら処理終了
+        if k == 27: break
+    sleep(3)
+
+if __name__ == "__main__":
+    move_info = Chattering()
+    timer = TimeMng()
+    cap = cv2.VideoCapture(0)
+    avg = None
+
+    timer.update()
+    while True:
+        ret, frame = cap.read()
+        # フレーム取得できなかったらエラーと判断
+        if not ret:
+            print("ret is None")
+            alerm(880)
+            continue
+
+        # グレースケールに変換
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # 比較用フレーム取得
+        if avg is None:
+            avg = gray.copy().astype("float")
+            continue
+
+        # 現在のフレームと移動平均との差を計算
+        cv2.accumulateWeighted(gray, avg, 0.6)
+        frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
+
+        # デルタ画像を閾値処理を行う
+        thresh = cv2.threshold(frameDelta, 15, 255, cv2.THRESH_BINARY)[1]
+
+        cv2.imshow("thresh", thresh)
+
+        cnt = cv2.countNonZero(thresh)
+        if cnt > 500:
+            move_info.add(True)
+        else:
+            move_info.add(False)
+
+        now = datetime.now()
+        # 動いた！
+        if move_info.is_allTrue():
+            # タイマ初期化
+            print(timer.get_timedelta(now))
+            timer.update()
+
+        # 5分以上動きがなかった
+        if timer.is_over(now):
+            print(timer.get_timedelta(now))
+            # 音を鳴らす
+            alerm(440)
+            # タイマ初期化
+            timer.update()
+
+        # Esc押されたら処理終了
+        k = cv2.waitKey(500)
+        if k == 27: break
+
+    cap.release()
+    cv2.destroyAllWindows()
